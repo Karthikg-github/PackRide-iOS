6a95479c272fb38d26935eabedcdadbca1c7ea9f
Karthik <karthikgundavarapu@gmail.com>
2026-05-20 23:31:59 -0400

    Initial Commit

---
+++ PackRide/PackRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackRideApp.swift
+//  PackRide
+//
+//  Created by Karthik Gundavarapu on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PackRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
